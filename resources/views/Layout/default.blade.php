4722f098a3bea5da464469a82423ea8e18493124
ibnushevayanto <ibnushevayanto@gmail.com>
2020-06-21 14:35:41 +0700

    change title

--- resources/views/Layout/default.blade.php
+++ resources/views/Layout/default.blade.php
@@ -14,8 +14,8 @@
 
 <body>
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="{{ route('landing') }}">ibnushevayanto</a>
+    <nav class="navbar navbar-expand-lg navbar-light bg-white">
+        <a class="navbar-brand" href="{{ route('landing') }}">MyBlog</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
             aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
