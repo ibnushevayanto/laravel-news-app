f8b988572f0e3ed5ef560bf693497bf39ef2aa5f
ibnushevayanto <ibnushevayanto@gmail.com>
2020-07-06 02:33:11 +0700

    Add A Documentation

--- my_documentation.php
+++ my_documentation.php
@@ -2,14 +2,21 @@
 
 /*
 
-    * Note Mengenai Relation One To One And One To Many : 
-    ! One To One Relationship Insert Data :
+    * Note Mengenai Relation [One To One] And [One To Many] : 
+
+    ? Relationship Insert Data
     ? $author adalah primaryKey Sedangkan $profile adalah foreignKey
     ! 1. $author->profile()->save($profile);
     ! 2. $profile->author()->associate($author)->save();
 
     * Note Mengenai Relationship Existance
-    ! BlogPosts::has('comments')->get()
+
     ? Mencari Record BlogPosts Yang Sudah Memiliki Komentar
-    
+    ! BlogPosts::has('comments')->get()
+
+    ? Mencari BlogPosts Yang Mempunyai Komentar Lebih Dari 5
+    ! BlogPosts::has('comments', '>', '5')->get()
+
+    ? Mencari BlogPosts Yang Mempunyai Content Tertentu
+    ! BlogPosts::whereHas('comments', function($query){ $query->where('content', 'like', '%test%'); })->get()
 */
