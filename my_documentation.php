cdc22dc300367ae7ed14d27af502ffb56bc4e59c
ibnushevayanto <ibnushevayanto@gmail.com>
2020-07-06 17:28:45 +0700

    documentation abscence query

--- my_documentation.php
+++ my_documentation.php
@@ -9,14 +9,31 @@
     ! 1. $author->profile()->save($profile);
     ! 2. $profile->author()->associate($author)->save();
 
+    // =======================================================================================================================
+
     * Note Mengenai Relationship Existance
 
     ? Mencari Record BlogPosts Yang Sudah Memiliki Komentar
-    ! BlogPosts::has('comments')->get()
+    ! BlogPosts::has('comments')->get();
 
     ? Mencari BlogPosts Yang Mempunyai Komentar Lebih Dari 5
-    ! BlogPosts::has('comments', '>', '5')->get()
+    ! BlogPosts::has('comments', '>', '5')->get();
 
-    ? Mencari BlogPosts Yang Mempunyai Content Tertentu
-    ! BlogPosts::whereHas('comments', function($query){ $query->where('content', 'like', '%test%'); })->get()
+    ? Mencari Komentar Pada BlogPosts Yang Mempunyai Content Tertentu
+    ! BlogPosts::whereHas('comments', function($query){ 
+    !    $query->where('content', 'like', '%test%'); 
+    ! })->get();
+
+    // =======================================================================================================================
+
+    * Note Mengenai Relationship Absence
+
+    ? Mencari Record Yang Tidak Memiliki Komentar
+    ! BlogPosts::has('comments')->get();
+
+    ? Mencari Komentar Yang Tidak Memiliki Content Yang Diinginkan
+    ! BlogPosts::whereDoesntHave('comments', function($query){ 
+    !    $query->where('content', 'like', '%abc%'); 
+    ! })->get();
+    
 */
